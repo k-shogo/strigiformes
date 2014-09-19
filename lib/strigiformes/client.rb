354509e13eb167290ea2f362b4cc8238431a06fe
Shogo Kawaguchi <platycod0n.ramosa@gmail.com>
2014-09-19 23:40:25 +0900

    publisherのひな形を追加

--- lib/strigiformes/client.rb
+++ lib/strigiformes/client.rb
@@ -6,7 +6,7 @@ module Strigiformes
     attr_accessor *Configuration::VALID_OPTIONS_KEYS
 
     def initialize(options = {})
-      options = self.class.options.merge(options)
+      options = Strigiformes.options.merge(options)
       Configuration::VALID_OPTIONS_KEYS.each do |key|
         send("#{key}=", options[key])
       end
--- lib/strigiformes/configuration.rb
+++ lib/strigiformes/configuration.rb
@@ -5,6 +5,9 @@ module Strigiformes
       :access_key, :secret_key
     ].freeze
 
+    DEFAULT_ACCESS_KEY = nil
+    DEFAULT_SECRET_KEY = nil
+
     attr_accessor *VALID_OPTIONS_KEYS
 
     def self.extended(base)
@@ -20,6 +23,12 @@ module Strigiformes
       VALID_OPTIONS_KEYS.inject({}) do |options, key|
         options.merge!(key => send(key))
       end
+    end
+
+    def reset
+      self.access_key = DEFAULT_ACCESS_KEY
+      self.secret_key = DEFAULT_SECRET_KEY
+      self
     end
 
   end
